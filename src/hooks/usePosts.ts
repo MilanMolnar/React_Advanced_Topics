02b660a5ef933154d1ad0ed36a82b2cefcfcfa0b
Molnar Milan <molnarmilan.cc@gmail.com>
2024-06-09 20:24:42 +0200

    implements infinite queries with react query

--- src/hooks/usePosts.ts
+++ src/hooks/usePosts.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
 
 interface Post {
   id: number;
@@ -9,28 +9,27 @@ interface Post {
 }
 
 interface PostQuery {
-  page: number;
   pageSize: number;
 }
 
 const usePosts = (query: PostQuery) => {
-  const fetchPosts = () => {
-    return axios
-      .get<Post[]>("https://jsonplaceholder.typicode.com/posts", {
-        params: {
-          _start: (query.page - 1) * query.pageSize,
-          _limit: query.pageSize,
-        },
-      })
-      .then((res) => res.data);
-  };
-
-  return useQuery<Post[], Error>({
+  return useInfiniteQuery<Post[], Error>({
     //              /users/1/posts dependency chain
     queryKey: ["posts", query],
-    queryFn: fetchPosts,
+    queryFn: ({ pageParam = 1 }) =>
+      axios
+        .get<Post[]>("https://jsonplaceholder.typicode.com/posts", {
+          params: {
+            _start: (pageParam - 1) * query.pageSize,
+            _limit: query.pageSize,
+          },
+        })
+        .then((res) => res.data),
     staleTime: 60 * 1000, // 60 sec
     keepPreviousData: true,
+    getNextPageParam: (lastPage, allPages) => {
+      return lastPage.length > 0 ? allPages.length + 1 : undefined;
+    },
   });
 };
 export default usePosts;
