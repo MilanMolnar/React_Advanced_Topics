e16a6841666b9485ba06cddec92ae341a2d43592
Molnar Milan <molnarmilan.cc@gmail.com>
2024-06-12 21:42:13 +0200

    implements route based rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,7 @@
 import "./App.css";
-import NavBar from "./state-management/NavBar";
-import HomePage from "./state-management/HomePage";
-import AuthProvider from "./state-management/AuthProvider";
-import { TasksProvider } from "./state-management/tasks";
 
 function App() {
-  return (
-    <AuthProvider>
-      <TasksProvider>
-        <NavBar />
-        <HomePage />
-      </TasksProvider>
-    </AuthProvider>
-  );
+  return <></>;
 }
 
 export default App;
